dc3c44260644590075ae97ad16fccfc155e5a135
jacobfederer <contact@jacobfederer.com>
2022-12-01 16:59:26 +0100

    Combine reader, writer and calculator in main function

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,15 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.calculator.SmallestDistanceCalculator;
+import de.exxcellent.challenge.reader.CSVDistanceReader;
+import de.exxcellent.challenge.reader.DataInconsistentException;
+import de.exxcellent.challenge.reader.DistanceReader;
+import de.exxcellent.challenge.writer.DistanceConsoleWriter;
+import de.exxcellent.challenge.writer.DistanceWriter;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -10,16 +20,41 @@ public final class App {
 
     /**
      * This is the main entry method of your program.
+     *
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
+        if (args.length == 0) {
+            return;
+        }
 
-        // Your preparation code …
+        boolean footballMode = args[0].equalsIgnoreCase("--football");
+        var filePathIndex = 0;
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+        DistanceReader reader;
+        DistanceWriter writer;
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+        if (footballMode) {
+            reader = new CSVDistanceReader("Team", "Goals", "Goals Allowed");
+            writer = new DistanceConsoleWriter(System.out, System.err, "Team with smallest goal spread       : %s%n");
+            filePathIndex += 1;
+        } else {
+            reader = new CSVDistanceReader("Day", "MnT", "MxT");
+            writer = new DistanceConsoleWriter(System.out, System.err, "Day with smallest temperature spread : %s%n");
+        }
+
+        var path = Paths.get(args[filePathIndex]);
+
+        try {
+            var distances = reader.readDistancesFromUri(path.toUri());
+
+            var distance = SmallestDistanceCalculator.calculateSmallestDistance(distances);
+            
+            if (distance != null) {
+                writer.writeDistance(distance);
+            }
+        } catch (IOException | DataInconsistentException e) {
+            writer.writeParseError(e);
+        }
     }
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,31 +1,70 @@
 package de.exxcellent.challenge;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Example JUnit 5 test case.
+ *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
 
-    private String successLabel = "not successful";
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
 
     @BeforeEach
-    void setUp() {
-        successLabel = "successful";
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+
+    @Test
+    void itShouldOutputDay14WhenRunningWithSampleWeatherCsv() {
+
+        App.main("src/main/resources/de/exxcellent/challenge/weather.csv");
+
+        assertEquals("Day with smallest temperature spread : 14\n", outContent.toString());
+    }
+
+
+    @Test
+    void itShouldLogErrorWhenRunningWithUnknownFile() {
+        App.main("/unknown.csv");
+
+        assertEquals("", outContent.toString());
+
+        assertEquals("Unable to parse input file, err: java.io.FileNotFoundException: /unknown.csv (No such file or directory)", errContent.toString());
     }
 
     @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
+    void itShouldOutputAstonVillakWhenRunningWithSampleFootballCsvAndFootballFlag() {
+        App.main("--football", "src/main/resources/de/exxcellent/challenge/football.csv");
+
+        assertEquals("Team with smallest goal spread       : Aston_Villa\n", outContent.toString());
     }
 
     @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
+    void itShouldExitQuietlyWhenNoArgumentIsProvided() {
+        App.main();
+
+        assertEquals("", outContent.toString());
+        assertEquals("", errContent.toString());
     }
 
 }
